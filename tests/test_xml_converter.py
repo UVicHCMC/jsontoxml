ca947c612e3911c2e6ce1876aa33112d9da19508
denizaydin <deniznaydin@gmail.com>
2024-11-21 09:53:56 -0800

    interim changes

---
+++ tests/test_xml_converter.py
@@ -0,0 +1,12 @@
+import pytest
+import sys
+from unittest.mock import MagicMock, patch, mock_open
+import os
+
+# Now import the functions you want to test
+
+from src.json_to_xml import Actor
+
+
+def test_xml_converter():
+    assert True
