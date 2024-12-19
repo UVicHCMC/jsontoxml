997ad18516c528c8fd00621b250b0a6048ee5b5c
denizaydin <deniznaydin@gmail.com>
2024-12-19 14:36:29 -0800

    updates from Marine's repo; reworked the main code snippet for comedians and created a working sample of writing to XML using nodes in ElementTree directly instead of writing to a string and converting that to XML.

--- src/json_to_xml.py
+++ src/json_to_xml.py
@@ -2,80 +2,56 @@ import json
 import xml.etree.ElementTree as ET
 from typing import Optional
 
-# Open and read the JSON file
-with open('actors.json', 'r') as file:
-    data = json.load(file)
 
-
-class Actor:
-    def __init__(self, id: Optional[int], pseudonym: Optional[str], pseudonym_number: Optional[int],
-                 honorific: Optional[str],
-                 first_name: Optional[str], last_name: Optional[str], alias: Optional[str], status: Optional[str],
-                 entree: Optional[int],
-                 societariat: Optional[int], depart: Optional[int], debut: Optional[list[int]],
-                 notes: Optional[str]) -> None:
-        self.notes = notes
-        self.debut = debut
-        self.depart = depart
-        self.societariat = societariat
-        self.entree = entree
-        self.status = status
-        self.alias = alias
-        self.last_name = last_name
-        self.first_name = first_name
-        self.honorific = honorific
-        self.pseudonym_number = pseudonym_number
-        self.pseudonym = pseudonym
+class Comedian:
+    def __init__(self, id: Optional[int], pseudonym: Optional[str], variations: Optional[str],
+                 first_name: Optional[str], last_name: Optional[str], title: Optional[str], female: Optional[bool],
+                 status: Optional[str], entry: Optional[int], society: Optional[int], departure: Optional[int]) -> None:
         self.id = id
+        self.pseudonym = pseudonym
+        self.variations = variations
+        self.first_name = first_name
+        self.last_name = last_name
+        self.title = title
+        self.female = female
+        self.status = status
+        self.entry = entry
+        self.society = society
+        self.departure = departure
 
 
 def main():
-    tree = ET.parse('example.xml')
+    tree = ET.parse('../output/example_prosopohgraphy.xml')
     ET.register_namespace('', "http://www.tei-c.org/ns/1.0")
     root = tree.getroot()
 
-    actors = list()
+    comedians = list()
+    # flatten "data" to get rid of the awkward key
+    # Open and read the JSON file
+    with open('../json_exports/comédiens_CF.json', 'r') as file:
+        data = json.load(file)
 
-    for entry in data:
-        actor = Actor
-        actor.id = entry['id']
-        actor.pseudonym = entry['pseudonym']
-        actor.pseudonym_number = entry['pseudonym_number']
-        actor.honorific = entry['honorific']
-        actor.first_name = entry['first_name']
-        actor.last_name = entry['last_name']
-        actor.alias = entry['alias']
-        actor.status = entry['status']
-        actor.entree = entry['entree']
-        actor.societariat = entry['societariat']
-        actor.depart = entry['depart']
-        actor.debut = entry['debut']
-        actor.notes = entry['notes']
-        actors.append(actor)
+    for entry_list in data.values():
+        for entry in entry_list:
+            comedian = Comedian(entry['id'], entry['pseudonyme'], entry['variations'], entry['prénom'], entry['nom'],
+                                entry['titre'], entry['féminin'], entry['statut'], entry['entrée'], entry['sociétariat'],
+                                entry['départ'])
+            comedians.append(comedian)
 
-    spot = root.find(".//{http://www.tei-c.org/ns/1.0}people")
+    listPerson = root.find(".//{http://www.tei-c.org/ns/1.0}listPerson")
+    # add one <persName> node per <person> with all the subnodes
+    # add the other fields as top-level nodes to <person>
+    # <person> has an xml:id that is auto-generated
 
-    for actor in actors:
-        text_to_write = "\n<person>\n"
-        text_to_write += f"<id>{actor.id}</id>\n"
-        text_to_write += f"<pseudonym>{actor.pseudonym}</pseudonym>\n"
-        text_to_write += f"<pseudonymNumber>{actor.pseudonym_number}</pseudonymNumber>\n"
-        text_to_write += f"<honorific>{actor.honorific}</honorific>\n"
-        text_to_write += f"<firstName>{actor.first_name}</firstName>\n"
-        text_to_write += f"<lastName>{actor.last_name}</lastName>\n"
-        text_to_write += f"<alias>{actor.alias}</alias>\n"
-        text_to_write += f"<status>{actor.status}</status>\n"
-        text_to_write += f"<entree>{actor.entree}</entree>\n"
-        text_to_write += f"<societariat>{actor.societariat}</societariat>\n"
-        text_to_write += f"<depart>{actor.depart}</depart>\n"
-        text_to_write += f"<debut>{actor.debut}</debut>\n"
-        text_to_write += f"<notes>{actor.notes}</notes>\n"
-        text_to_write += "</person>\n"
-        spot.append(ET.fromstring(text_to_write))
+    for comedian in comedians:
+        person = ET.SubElement(listPerson, 'person')
+        persName = ET.SubElement(person, 'persName')
+        forename = ET.SubElement(persName, 'forename')
+        forename.text = comedian.first_name
 
         # Prettify output (requires Python 3.9)
     ET.indent(tree)
-    tree.write('example_mod.xml', encoding='UTF-8', xml_declaration=True,
+    tree.write('../output/example_mod.xml', encoding='UTF-8', xml_declaration=True,
                method='xml')
 
 
